4edcc9efe25765043caa28fd637303b2c4f60e0a
Sujit Srk <sujit2050@hotmail.com>
2014-05-21 20:01:03 +0530

    Programming Assignment 2 by Sujit Sarkar

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions demonstrates the use of cache for reducing time for calculation of repetetive tasks
+## Our task here is to calculate the inverse of a matrix and store it in a cache 
+## by making use of scoping rules in R
+ 
 
-## Write a short comment describing this function
+##The function makeCacheMatix return a list containing 4 functions viz function to  
+##set the value of the matrix (which is passed as argument)
+##get the value of the matrix
+##set the value of the inverse of the matrix in the parent environemtn (function makeCacheMatrix )
+##get the value of the inverse of the matrix (by retrieving the value in the globali variable in parent enviroment set by the set function)
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -21,7 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the matrix
+## created with the above function. 
+##However, it first checks to see if the inverse has already been calculated.
+## If so, it gets the inverse from the cache and skips the computation. 
+##Otherwise, it calculates the inverse of the matrix and 
+##sets the value of the inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
